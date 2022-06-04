8ca7d034057c6d22f3352bb8388a6bcaae5f50cd
Sourcery AI <>
2022-06-04 20:51:34 +0000

    'Refactored by Sourcery'

--- database.py
+++ database.py
@@ -9,20 +9,17 @@ class Chat:
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
-    def is_ai_chat(chat_id):
-        if kuki_db.find_one({"chat_id": chat_id}):
-            return True
-        else:
-            return False
+    def is_ai_chat(self):
+        return bool(kuki_db.find_one({"chat_id": self}))
 
-    def add_chat(chat_id):
-        if not Chat.is_ai_chat(chat_id):
-            kuki_db.insert_one({"chat_id": chat_id})
+    def add_chat(self):
+        if not Chat.is_ai_chat(self):
+            kuki_db.insert_one({"chat_id": self})
         else:
             return
 
-    def rm_chat(chat_id):
-        if Chat.is_ai_chat(chat_id):
-            kuki_db.delete_one({"chat_id": chat_id})
+    def rm_chat(self):
+        if Chat.is_ai_chat(self):
+            kuki_db.delete_one({"chat_id": self})
         else:
             return
